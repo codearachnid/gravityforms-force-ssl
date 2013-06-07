328dfe1766ad311fc95175616b9659941852a434
Timothy Wood <tim@imaginesimplicity.com>
2013-06-07 17:21:40 -0400

    init code commit

---
+++ gravityforms-force-ssl.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Plugin Name: Gravity Forms: Force SSL
+ * Plugin URI:
+ * Description: An addon to Gravity Forms to add an option to force your forms to be loaded SSL only.
+ * Version: 1.0
+ * Author: Timothy Wood (@codearachnid)
+ * Author URI: http://www.codearachnid.com
+ * Author Email: tim@imaginesimplicity.com
+ * Text Domain: 'gf-force-ssl' 
+ * License:
+ * 
+ *     Copyright 2013 Imagine Simplicity (tim@imaginesimplicity.com)
+ *     License: GNU General Public License v3.0
+ *     License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ * 
+ * @author codearachnid
+ * 
+ */
