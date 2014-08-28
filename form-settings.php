8e52b20263a9c80484420f9f6007eeb9d938849b
Timothy Wood <tim@imaginesimplicity.com>
2014-08-28 07:26:22 -0400

    Fixed tooltip popup

--- form-settings.php
+++ form-settings.php
@@ -7,7 +7,7 @@ if ( !defined( 'ABSPATH' ) )
 <tr>
 	<th>
 		<?php _e( 'Force SSL', 'gf-force-ssl' ); ?>
-		<a href="#" onclick="return false;" class="tooltip tooltip_form_require_login" tooltip="<h6><?php _e( 'Require page to be https', 'gf-force-ssl' ); ?></h6><?php _e( 'Check this option to require the page to be https to view this form', 'gf-force-ssl' ); ?>">(?)</a>
+		<a href="#" onclick="return false;" class="tooltip tooltip_form_require_login" tooltip="<h6><?php _e( 'Require page to be https', 'gf-force-ssl' ); ?></h6>"><a href='#' onclick='return false;' class='gf_tooltip tooltip' title='<h6><?php _e( 'Require page to be https', 'gf-force-ssl' ); ?></h6><?php _e( 'Check this option to require the page to be https to view this form. Will redirect to the SSL version of this page.<br /><br /><b>Note:</b> will preserve any query string parameters.', 'gf-force-ssl' ); ?>'><i class='fa fa-question-circle'></i></a></a>
 	</th>
 	<td>
 		<input type="checkbox" value="1" name="force_ssl" <?php checked( rgar( $form, 'force_ssl' ) ); ?> />
